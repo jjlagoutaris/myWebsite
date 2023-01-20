6ac08f36ad3667cb9e10ac97ab1da0b9da3f415c
John Lagoutaris <jjlagoutaris@gmail.com>
2023-01-20 04:51:34 -0500

    Update icons, clean up file structure

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -1,39 +1,46 @@
-import email from '../assets/email.png';
-import mobile from '../assets/mobile.png';
-import api from '../assets/api.png';
-import cpp from '../assets/cpp.png';
-import css from '../assets/css.png';
-import git from '../assets/git.png';
-import html from '../assets/html.png';
-import javascript from '../assets/javascript.png';
-import node from '../assets/node.png';
-import python from '../assets/python.png';
-import react from '../assets/react.png';
-import sass from '../assets/sass.png';
-import typescript from '../assets/typescript.png';
+// tech icons
+import react from '../assets/icons/react.png';
+import sass from '../assets/icons/sass.png';
+import typescript from '../assets/icons/typescript.png';
+// import css from '../assets/icons/css.png';
+// import git from '../assets/icons/git.png';
+// import html from '../assets/icons/html.png';
+// import javascript from '../assets/icons/javascript.png';
+// import python from '../assets/icons/python.png';
+// import npm from "../assets/icons/npm.png";
+// import nodejs from "../assets/icons/node-js.png";
+// import express from "../assets/icons/express.png";
+// import mongoDB from "../assets/icons/mongodb.png";
+// import sanity from "../assets/icons/sanity.png";
 
+// misc icons
+import bgg from "../assets/icons/bgg.jpg";
+import linkedin from "../assets/icons/linkedin.png";
+
+// general images
 import profile from '../assets/profile.png';
 import circle from '../assets/circle.svg';
 import logo from '../assets/myLogo.png';
-
-import bgg from "../assets/bgg.jpg";
-import linkedin from "../assets/linkedin.png";
+import email from '../assets/email.png';
+import mobile from '../assets/mobile.png';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   email,
   mobile,
-  api,
-  cpp,
-  css,
-  git,
-  html,
-  javascript,
-  node,
-  python,
   react,
   sass,
   typescript,
+  // css,
+  // git,
+  // html,
+  // javascript,
+  // python,
+  // npm,
+  // nodejs,
+  // express,
+  // mongoDB,
+  // sanity,
   profile,
   circle,
   logo,
